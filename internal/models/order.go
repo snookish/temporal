a28d8bcbd49a15c3d0e400281a768aee000b45ce
snookish <iamnilotpaldeka@gmail.com>
2026-02-14 12:36:42 +0530

    added in initial types just to set things up, idk it's correct or not atp

---
+++ internal/models/order.go
@@ -0,0 +1,51 @@
+package models
+
+type OrderStatus string
+
+const (
+	OrderStatusPending                   OrderStatus = "PENDING"
+	OrderStatusPaymentFailed             OrderStatus = "PAYMENT_FAILED"
+	OrderStatusPaymentSucceeded          OrderStatus = "PAYMENT_SUCCEEDED"
+	OrderStatusReserveInventoryFailed    OrderStatus = "RESERVE_INVENTORY_FAILED"
+	OrderStatusReserveInventorySucceeded OrderStatus = "RESERVE_INVENTORY_SUCCEEDED"
+)
+
+type PaymentMethod string
+
+const (
+	PaymentMethodUPI  PaymentMethod = "UPI"
+	PaymentMethodCard PaymentMethod = "CARD"
+)
+
+type ProcessOrderCommand struct {
+	OrderID       string
+	CustomerID    string
+	Items         []OrderItem
+	Amount        Money
+	PaymentMethod PaymentMethod
+}
+
+type Money struct {
+	Currency string
+	Amount   float64
+}
+
+type OrderItem struct {
+	Name      string
+	ProductID string
+	Quantity  uint
+	UnitPrice Money
+}
+
+type OrderState struct {
+	OrderID string
+	Status  OrderStatus
+}
+
+func (o *OrderState) Is(status OrderStatus) bool {
+	return o.Status == status
+}
+
+func (o *OrderState) IsPaid() bool {
+	return o.Status == OrderStatusPaymentSucceeded
+}
